52545bfe767a43fdc9b8dddef0d5fdd834e642d8
namiwa <khairul.nasri895@gmail.com>
2020-11-04 03:36:10 +0800

    init: flask
    clean up of server side code-base, and single local jupyter notebook for daily wind data

--- app/app.py
+++ app/app.py
@@ -2,9 +2,19 @@ from flask import Flask
 from flask_restful import Api, Resource
 import os
 import tensorflow as tf
+from flask_restful import reqparse
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', }
+INTERIM_MODEL_DIR = 'interim_model'
 
 app = Flask(__name__)
 api = Api(app)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -13,13 +23,19 @@ def index():
 
 
 class UserApi(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('file', type=str, location='json')
+        super(UserApi, self).__init__()
+
     def get(self, id):
         return {'task': 'testing'}
 
-    def put(self, id):
-        pwd = os.getcwd()
-        model = tf.saved_model.load(os.path.join(pwd, 'app'))
-        return {'path': str(dir(model))}
+    def post(self, id):
+        pwd = os.path.dirname(os.path.abspath(__file__))
+        model = tf.saved_model.load(os.path.join(pwd, INTERIM_MODEL_DIR))
+        args = self.reqparse.parse_args()
+        return {'path': args['file']}
 
     def delete(self, id):
         pass
